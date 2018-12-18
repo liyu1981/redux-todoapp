365b4cec47cca2964581def65f1c6683b9967b9c
LI Yu <liyuhk@fb.com>
2018-12-18 12:19:36 +1100

    add flow checking

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* @flow */
+
 import 'babel-polyfill';
 
 import React from 'react';
@@ -6,25 +8,27 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 
-const history = createBrowserHistory();
-
 import configureStore from './store/configureStore';
 import App from './containers/App';
 
+const history = createBrowserHistory();
 // Can pass an initial state
 // const store = configureStore(initialState);
 const store = configureStore();
+const rootElem = document.getElementById('root');
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <div>
-        <Route exact path='/' component={App} />
-        <Route path='/active' component={App}/>
-        <Route path='/completed' component={App}/>
-        <Route path='/all' component={App}/>
-      </div>
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
+if (rootElem) {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router history={history}>
+        <div>
+          <Route exact path='/' component={App} />
+          <Route path='/active' component={App}/>
+          <Route path='/completed' component={App}/>
+          <Route path='/all' component={App}/>
+        </div>
+      </Router>
+    </Provider>,
+    rootElem
+  );
+}
